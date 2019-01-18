7c77a1f7b0540b1a12e5f237685ad8908333ffca
ticture <32947550+1436294280@users.noreply.github.com>
2019-01-18 15:47:54 +0800

    Update label.py

--- chapter_1/label.py
+++ chapter_1/label.py
@@ -7,7 +7,7 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 # 看前20张训练图片的label
 for i in range(201):
     # 得到one-hot表示，形如(0, 1, 0, 0, 0, 0, 0, 0, 0, 0)
-    one_hot_label = mnist.train.labels[i, :]
+    one_hot_label = mnist.train.labels[i+2, :]
     # 通过np.argmax我们可以直接获得原始的label
     # 因为只有1位为1，其他都是0
     label = np.argmax(one_hot_label)
